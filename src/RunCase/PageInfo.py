0184ee9966297e4c25417a9fcba986797fccdb10
Soap Ye - CHN <218838@hk.fwd.com>
2023-04-18 17:12:42 +0800

    更新页面板块

--- src/RunCase/PageInfo.py
+++ src/RunCase/PageInfo.py
@@ -1,15 +1,16 @@
-# 通用按钮图片字典
-common_button_image_dict = {
+# 通用按钮
+common_button_dict = {
     "next": "next.png",  # 下一步
     "back": "back.png",  # 返回
     "cancel": "cancel.png",  # 取消
     "close": "close.png",  # 关闭
+    "skip": "skip.png",  # 跳过
     "white_comfirm": "white_comfirm.png",  # 白色确认按钮
     "blue_comfirm": "blue_comfirm.png",  # 蓝色确认按钮
 }
 
-# 各大板块主要页面图片字典
-page_image_dict = {
+# 各大板块主要页面
+page_dict = {
     "main": "main_page.png",  # 我的主页
     "mission": "mission_page.png",  # 任务
     "shop": "shop_page.png",  # 商店
@@ -20,4 +21,56 @@ page_image_dict = {
     "arana": "arana_page.png",  # 竞技场
     "princess_arana": "princess_arana_page.png",  # 公主竞技场
     "department": "department_page.png",  # 公会之家
+    "dungeons": "dungeons_page.png",  # 地下城
+}
+
+# 行会
+
+# 公会之家
+department_dict = {
+    "collect_all": "collect_all_deparment_strength.png"  # 全部收取
+}
+
+# 任务
+mission_dict = {
+
+}
+
+# 商店
+shop_dict = {
+
+}
+
+# 探索
+search_dict = {
+    "": "experience_page.png",
+    "": "mana_page.png",
+    "": "mana_page.png",
+    "": "mana_page.png",
+}
+
+# 调查
+invest_dict = {
+    "": "temple_page.png",
+    "": "miracle_page.png",
+    "": "miracle_survey_level_one.png",
+    "": "miracle_survey_level_two.png",
+    "": "miracle_survey_level_three.png",
+    "": "temple_survey_level_one.png",
+    "": "temple_survey_level_two.png",
+}
+
+# 竞技场
+arana_dict = {
+    "": "colloct_arana_coin.png"
+}
+
+# 公主竞技场
+princess_arana_dict = {
+    "": "colloct_arana_coin.png"
+}
+
+# 地下城
+dungeons_dict = {
+
 }
