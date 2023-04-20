c0dd6419f53654ca6076a3c91f720c49b34861e1
Vsoapmac <17724147277@163.com>
2023-04-20 23:47:47 +0800

    更新逻辑

--- src/RunCase/TestDepartment.py
+++ src/RunCase/TestDepartment.py
@@ -6,4 +6,9 @@ class TestDepartment(ClientBasic):
 
     def test_collect_deparment_strength(self):
         """收取工会之家的体力,开始:主页,结束:公会之家"""
-        pass
+        touch(Template(r"tpl1682005597629.png", record_pos=(0.437, 0.162), resolution=(1280, 720))) # 全部收取
+        touch(Template(r"tpl1682005608816.png", record_pos=(-0.438, -0.141), resolution=(1280, 720))) # 2层
+        touch(Template(r"tpl1682005620883.png", record_pos=(-0.438, -0.191), resolution=(1280, 720))) # 3层
+
+
+
--- src/RunCase/TestShop.py
+++ src/RunCase/TestShop.py
@@ -6,9 +6,13 @@ class TestShop(ClientBasic):
 
     def test_buy_shop_potion(self):
         """购买主商店的所有经验药水,开始:首页,结束:首页"""
-        pass
+        touch(Template(r"tpl1682005455115.png", record_pos=(-0.148, -0.151), resolution=(1280, 720))) # 药剂
+        touch(Template(r"tpl1682005467781.png", record_pos=(0.221, -0.149), resolution=(1280, 720))) # 全部
+        touch(Template(r"tpl1682005495636.png", record_pos=(0.395, -0.152), resolution=(1280, 720))) # 全选
+        touch(Template(r"tpl1682005516815.png", record_pos=(0.279, 0.174), resolution=(1280, 720))) # 批量购入
 
     @pytest.skip(bool=True)
     def test_buy_shop_upgrate(self):
         """购买主商店的所有升级材料,开始:首页,结束:首页"""
-        pass
+        touch(Template(r"tpl1682005434629.png", record_pos=(0.035, -0.151), resolution=(1280, 720))) # 精炼石
+
