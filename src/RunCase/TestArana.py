7ee395d71d71bf7e50fd2134e0944ecfb35d5e28
Vsoapmac <17724147277@163.com>
2023-04-20 23:51:36 +0800

    更新逻辑

--- src/RunCase/TestArana.py
+++ src/RunCase/TestArana.py
@@ -6,7 +6,7 @@ class TestPreArana(ClientBasic):
 
     def test_arana(self):
         """竞技场,开始:冒险,结束:冒险"""
-        touch(0.612*elf.w,0.302*self.h) # 点击第一个人
+        touch(0.612*self.w,0.302*self.h) # 点击第一个人
         touch(Template(r"tpl1681996844208.png", record_pos=(-0.192, 0.073), resolution=(1280, 720))) # 收取
         touch(Template(r"tpl1681996858374.png", record_pos=(-0.384, 0.164), resolution=(1280, 720))) # 对战覆历
         touch(Template(r"tpl1681996885521.png", record_pos=(-0.231, 0.165), resolution=(1280, 720))) # 防守设定
@@ -14,7 +14,7 @@ class TestPreArana(ClientBasic):
 
     def princess_arana(self):
         """公主竞技场,开始:冒险,结束:冒险"""
-        touch(0.612*elf.w,0.302*self.h) # 点击第一个人
+        touch(0.612*self.w,0.302*self.h) # 点击第一个人
         touch(Template(r"tpl1681996844208.png", record_pos=(-0.192, 0.073), resolution=(1280, 720))) # 收取
         touch(Template(r"tpl1681996951013.png", record_pos=(-0.385, 0.175), resolution=(1280, 720))) # 对战覆历
         touch(Template(r"tpl1681996968893.png", record_pos=(-0.231, 0.175), resolution=(1280, 720))) # 防守设定
---
+++ src/RunCase/TestDungeons.py
@@ -0,0 +1,8 @@
+from Common.ClientBasic import *
+
+
+class TestDungeons(ClientBasic):
+    """地下城"""
+
+    def test_dungeons(self):
+        pass
