cbe98a182152d0e22159e1e61a980d9a85301a43
Soap Ye - CHN <218838@hk.fwd.com>
2023-04-24 11:59:13 +0800

    增加logging.info指示当前进度

--- src/RunCase/TestInvest.py
+++ src/RunCase/TestInvest.py
@@ -7,41 +7,53 @@ class TestInvest(ClientBasic):
 
     def test_miracle_survey_level_one(self, miracle_page):
         """圣迹调查关卡1级,开始:圣迹调查,结束:圣迹调查"""
+        logging.info("开始圣迹调查关卡1级")
         touch(Template(self.shot_path + self.__page_dict["miracle_survey_level_one"], record_pos=(0.252, 0.071),
                        resolution=(1280, 720), threshold=0.9))  # 圣迹调查关卡1级
         self.__start_invest()
+        logging.info("圣迹调查关卡1级调查完毕")
 
     def test_miracle_survey_level_two(self, miracle_page):
         """圣迹调查关卡2级,开始:圣迹调查,结束:圣迹调查"""
+        logging.info("开始圣迹调查关卡2级")
         touch(Template(self.shot_path + self.__page_dict["miracle_survey_level_two"], record_pos=(0.255, -0.043),
                        resolution=(1280, 720), threshold=0.9))  # 圣迹调查关卡2级
         self.__start_invest()
+        logging.info("圣迹调查关卡2级调查完毕")
 
     def test_miracle_survey_level_three(self, miracle_page):
         """圣迹调查关卡3级,开始:圣迹调查,结束:圣迹调查"""
+        logging.info("开始圣迹调查关卡3级")
         touch(Template(self.shot_path + self.__page_dict["miracle_survey_level_three"], record_pos=(0.254, -0.159),
                        resolution=(1280, 720), threshold=0.9))  # 圣迹调查关卡3级
         self.__start_invest()
+        logging.info("圣迹调查关卡3级调查完毕")
 
     def test_temple_survey_level_one(self, temple_page):
         """神殿调查关卡1级,开始:神殿调查,结束:神殿调查"""
+        logging.info("开始神殿调查关卡1级")
         touch(Template(self.shot_path + self.__page_dict["temple_survey_level_one"], record_pos=(0.26, -0.042),
                        resolution=(1280, 720), threshold=0.9))  # 神殿调查关卡1级
         self.__start_invest()
+        logging.info("神殿调查关卡1级调查完毕")
 
     def test_temple_survey_level_two(self, temple_page):
         """神殿调查关卡2级,开始:神殿调查,结束:神殿调查"""
+        logging.info("开始神殿调查关卡2级")
         touch(Template(self.shot_path + self.__page_dict["temple_survey_level_two"], record_pos=(0.262, -0.156),
                        resolution=(1280, 720), threshold=0.9))  # 神殿调查关卡2级
         self.__start_invest()
+        logging.info("神殿调查关卡2级调查完毕")
 
     @pytest.fixture
     def miracle_page(self):
         """圣迹调查页面"""
+        logging.info("开始进入圣迹调查页面")
         # 首先检测是否在里面了
         is_in_page = True
         try:
             wait(Template(self.shot_path + self.__page_dict["piece_of_heart"], record_pos=(0.075, -0.13), resolution=(1280, 720), threshold=0.9), timeout=2) # 心碎图片
+            logging.info("检测到已经在圣迹调查页面，无需进入")
         except:
             is_in_page = False
         # 不在那就直接进去
@@ -53,17 +65,22 @@ class TestInvest(ClientBasic):
                 wait(Template(self.shot_path + self.__page_dict["miracle"], record_pos=(0.11, 0.065),
                               resolution=(1280, 720), threshold=0.9), timeout=2)  # 圣迹调查
             except:
+                logging.info("检测到不在调查页面，开始进入调查页面")
                 Page.invest_page()  # 否则进入调查页面，再进去
+                logging.info("进入调查页面完毕")
             touch(Template(self.shot_path + self.__page_dict["miracle"], record_pos=(0.11, 0.065),
                            resolution=(1280, 720), threshold=0.9))  # 圣迹调查
+            logging.info("进入圣迹调查页面完毕")
 
     @pytest.fixture
     def temple_page(self):
         """神殿调查页面"""
+        logging.info("开始进入神殿调查页面")
         # 首先检测是否在里面了
         is_in_page = True
         try:
             wait(Template(self.shot_path + self.__page_dict["cup_of_stars"], record_pos=(0.08, -0.127), resolution=(1280, 720), threshold=0.9), timeout=2) # 星球杯图片
+            logging.info("检测到已经在神殿调查页面，无需进入")
         except:
             is_in_page = False
         # 不在那就直接进去
@@ -73,12 +90,16 @@ class TestInvest(ClientBasic):
                 wait(Template(self.shot_path + self.__page_dict["miracle"], record_pos=(0.11, 0.065), resolution=(1280, 720), threshold=0.9), timeout=2)  # 圣迹调查
                 wait(Template(self.shot_path + self.__page_dict["temple"], record_pos=(0.341, 0.064), resolution=(1280, 720), threshold=0.9), timeout=2)  # 神殿调查
             except:
+                logging.info("检测到不在调查页面，开始进入调查页面")
                 Page.invest_page()  # 否则进入调查页面，再进去
+                logging.info("进入调查页面完毕")
             touch(Template(self.shot_path + self.__page_dict["temple"], record_pos=(0.341, 0.064),
                            resolution=(1280, 720), threshold=0.9))  # 神殿调查
+            logging.info("进入神殿调查页面完毕")
 
     def __start_invest(self):
         """使用扫荡券扫荡"""
+        logging.info("使用扫荡券扫荡")
         for i in range(4):
             touch(Template(self.shot_path + self.__page_dict["plus"], record_pos=(0.415, 0.062),
                            resolution=(1280, 720)))  # +号
@@ -103,17 +124,22 @@ class TestInvest(ClientBasic):
             try:
                 touch(Template(self.shot_path + self.__page_dict["skip_and_finish"], record_pos=(0.001, 0.216),
                                resolution=(1280, 720)))  # 跳过完毕
+                logging.info("跳过完毕")
             except:
                 pass
             # 正常扫荡
             CommonButton.white_comfirm()
             sleep(1)
+            logging.info("检测是否有限定商城开启，若有则处理")
             Page.detect_limit_shop_open()
+            logging.info("扫荡结束")
         else:
             """
             没有次数则跳过
             """
+            logging.info("没有扫荡次数，取消")
             CommonButton.cancel()
             sleep(1)
+        logging.info("取消当前页面")
         CommonButton.cancel()
         sleep(1)
--- src/RunCase/TestSearch.py
+++ src/RunCase/TestSearch.py
@@ -7,12 +7,14 @@ class TestSearch(ClientBasic):
 
     def test_experience_level_11(self, experience_page):
         """经验值关卡11级,开始:冒险,结束:冒险"""
+        logging.info("点击首行经验值关卡")
         touch([0.7 * self.w, 0.287 * self.h])  # 确保永远都是第一个
         sleep(2)
         self.__start_search()
 
     def test_mana_level_11(self, mana_page):
         """玛娜关卡11级,开始:冒险,结束:冒险"""
+        logging.info("点击首行mana关卡")
         touch([0.7 * self.w, 0.287 * self.h])  # 确保永远都是第一个
         sleep(2)
         self.__start_search()
@@ -20,45 +22,56 @@ class TestSearch(ClientBasic):
     @pytest.fixture
     def experience_page(self):
         """经验值关卡"""
-        is_in_page = True
+        logging.info("开始进入经验值关卡")
+        is_in_page = False
         try:
             wait(Template(self.shot_path + self.__page_dict["experience_potion"], record_pos=(0.071, -0.13),
                           resolution=(1280, 720), threshold=0.9), timeout=2)  # 经验值药水
-            is_in_page = False
+            is_in_page = True
+            logging.info("检测到已经在经验值关卡，无需进入")
         except:
             pass
-        if is_in_page:
+        if not is_in_page:
             try:
                 wait(Template(self.shot_path + self.__page_dict["experience"], record_pos=(0.109, 0.065),
                               resolution=(1280, 720), threshold=0.9), timeout=2)  # 经验值关卡
             except:
+                logging.info("检测到不在搜索页面，进入搜索页面")
                 Page.search_page()
+                logging.info("进入搜索页面完毕")
             touch(Template(self.shot_path + self.__page_dict["experience"], record_pos=(0.109, 0.065),
                            resolution=(1280, 720)))  # 经验值关卡
+            logging.info("进入经验值关卡完毕")
             sleep(2)
 
     @pytest.fixture
     def mana_page(self):
-        """玛娜"""
-        is_in_page = True
+        """玛娜关卡"""
+        logging.info("开始进入mana关卡")
+        is_in_page = False
         try:
             wait(Template(self.shot_path + self.__page_dict["mana_potion"], record_pos=(0.07, -0.127),
                           resolution=(1280, 720), threshold=0.9), timeout=2)  # mana药水
-            is_in_page = False
+            is_in_page = True
+            logging.info("检测到已经在mana关卡，无需进入")
         except:
             pass
-        if is_in_page:
+        if not is_in_page:
             try:
                 wait(Template(self.shot_path + self.__page_dict["mana"], record_pos=(0.34, 0.064),
                               resolution=(1280, 720), threshold=0.9), timeout=2)
             except:
+                logging.info("检测到不在搜索页面，进入搜索页面")
                 Page.search_page()
+                logging.info("进入搜索页面完毕")
             touch(Template(self.shot_path + self.__page_dict["mana"], record_pos=(0.34, 0.064),
                            resolution=(1280, 720)))  # mana关卡
+            logging.info("进入mana关卡完毕")
             sleep(2)
 
     def __start_search(self):
         """使用扫荡券扫荡"""
+        logging.info("使用扫荡券扫荡")
         touch(Template(self.shot_path + self.__page_dict["use_two_tickets"], record_pos=(0.284, 0.065),
                        resolution=(1280, 720)))  # 使用2张
         sleep(2)
@@ -73,9 +86,11 @@ class TestSearch(ClientBasic):
         """
         if has_remain:
             # 开始扫荡
+            logging.info("开始扫荡")
             try:
                 touch(Template(self.shot_path + self.__page_dict["skip_and_finish"], record_pos=(0.001, 0.216),
                                resolution=(1280, 720)))  # 跳过完毕
+                logging.info("跳过完毕")
             except:
                 pass
             # 结束扫荡，结果处理
@@ -83,18 +98,21 @@ class TestSearch(ClientBasic):
             try:
                 wait(Template(self.shot_path + self.__page_dict["going_to_mana_page"], record_pos=(-0.001, 0.218), resolution=(1280, 720), threshold=0.9), timeout=2) # 前往玛娜关卡
                 touch(Template(self.shot_path + self.__page_dict["going_to_mana_page"], record_pos=(-0.001, 0.218), resolution=(1280, 720), threshold=0.9)) # 前往玛娜关卡
+                logging.info("扫荡完毕，前往玛娜关卡")
             except:
                 pass
             # 进入探索首页(其次)
             try:
                 wait(Template(self.shot_path + self.__page_dict["going_to_search_page"], record_pos=(-0.001, 0.216), resolution=(1280, 720), threshold=0.9), timeout=2) # 进入探索首页
                 touch(Template(self.shot_path + self.__page_dict["going_to_search_page"], record_pos=(-0.001, 0.216), resolution=(1280, 720), threshold=0.9)) # 进入探索首页
+                logging.info("扫荡完毕，进入探索首页")
             except:
                 pass
             # 进入经验值关卡(最后)
             try:
                 wait(Template(self.shot_path + self.__page_dict["going_to_experience_page"], record_pos=(-0.001, 0.218), resolution=(1280, 720), threshold=0.9), timeout=2) # 前往经验值关卡
                 touch(Template(self.shot_path + self.__page_dict["going_to_experience_page"], record_pos=(-0.001, 0.218), resolution=(1280, 720), threshold=0.9)) # 前往经验值关卡
+                logging.info("扫荡完毕，进入经验值关卡")
             except:
                 pass
             sleep(1)
@@ -102,6 +120,7 @@ class TestSearch(ClientBasic):
             """
             没有次数则跳过
             """
+            logging.info("没有扫荡次数，返回")
             CommonButton.cancel()
             sleep(1)
 
