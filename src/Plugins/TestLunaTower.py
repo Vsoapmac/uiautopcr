ff70a82408d0009ead7b09756dc3b309e4a92cf6
Soap Ye - CHN <218838@hk.fwd.com>
2023-05-19 09:11:38 +0800

    1、增加通常按钮的等待机制 2、增加露娜塔插件的logger提示与关闭剧情的逻辑，修复一些小bug

--- src/Plugins/TestLunaTower.py
+++ src/Plugins/TestLunaTower.py
@@ -7,15 +7,32 @@ class TestLunaTower(ClientBasic):
 
     def test_luna_tower(self):
         """露娜塔通关"""
+        self.logger.info("使用通关队伍")
         wait(Template(self.shot_path + self.__page_dict["pass_level_team"], record_pos=(0.38, -0.175), resolution=(1280, 720), threshold=0.9), timeout=3) # 通关队伍
         touch(Template(self.shot_path + self.__page_dict["pass_level_team"], record_pos=(0.38, -0.175), resolution=(1280, 720), threshold=0.9)) # 通关队伍
         sleep(2)
         wait(Template(self.shot_path + self.__page_dict["use_luna_pass_team"], record_pos=(0.311, -0.138), resolution=(1280, 720)), timeout=3) # 使用（对坐标有严格要求）
         touch(Template(self.shot_path + self.__page_dict["use_luna_pass_team"], record_pos=(0.311, -0.138), resolution=(1280, 720))) # 使用（对坐标有严格要求）
         sleep(1)
+        self.logger.info("在实战中使用")
         touch(Template(self.shot_path + self.__page_dict["use_luna_pass_team_to_fight"], record_pos=(0.152, 0.106), resolution=(1280, 720), threshold=0.9)) # 在实战中使用
         sleep(2)
+        self.logger.info("战斗开始")
+        wait(Template(self.shot_path + page_info.common_button_dict["start_battle"], record_pos=(0.373, 0.191), resolution=(1280, 720)), timeout=5)  # 战斗开始
         touch(Template(self.shot_path + page_info.common_button_dict["start_battle"], record_pos=(0.373, 0.191), resolution=(1280, 720)))  # 战斗开始
-        touch(Template(self.shot_path + page_info.common_button_dict["start_battle"], record_pos=(0.373, 0.191), resolution=(1280, 720)))  # 战斗开始
+        sleep(2)
         wait(Template(self.shot_path + self.__page_dict["next_step"], threshold=0.9, record_pos=(0.366, 0.231), resolution=(1280, 720)), timeout=100)  # 下一步
+        self.logger.info("结束战斗")
         touch(Template(self.shot_path + self.__page_dict["next_step"], threshold=0.9, record_pos=(0.366, 0.231), resolution=(1280, 720)))  # 下一步
+        sleep(2)
+        wait(Template(self.shot_path + self.__page_dict["next_step"], threshold=0.9, record_pos=(0.366, 0.231), resolution=(1280, 720)), timeout=10)  # 下一步
+        self.logger.info("确认物资")
+        touch(Template(self.shot_path + self.__page_dict["next_step"], threshold=0.9, record_pos=(0.366, 0.231), resolution=(1280, 720)))  # 下一步
+        sleep(5) # 等待响应结束
+        try:
+            wait(Template(self.shot_path + page_info.common_button_dict["close"], threshold=0.9, record_pos=(0.0, 0.218), resolution=(1280, 720)), timeout=5)  # 关闭
+            touch(Template(self.shot_path + page_info.common_button_dict["close"], threshold=0.9, record_pos=(0.0, 0.218), resolution=(1280, 720)))  # 关闭
+            self.logger.info("关闭剧情通知")
+        except:
+            pass
+        self.logger.info("露娜塔当前阶层通关完毕")
--- src/RunCase/CommonButton.py
+++ src/RunCase/CommonButton.py
@@ -7,39 +7,47 @@ class CommonButton:
     """公共按钮"""
     __page_dict = page_info.common_button_dict
     __shot_path = PathUtils.getScriptShotPath()
+    __default_timeout = 5
 
     @classmethod
     def next(cls):
         """下一步"""
+        wait(Template(cls.__shot_path+cls.__page_dict["next"], record_pos=(0.351, 0.23), resolution=(1280, 720)), timeout=cls.__default_timeout) # 下一步
         touch(Template(cls.__shot_path+cls.__page_dict["next"], record_pos=(0.351, 0.23), resolution=(1280, 720))) # 下一步
 
     @classmethod
     def back(cls):
         """返回"""
+        wait(Template(cls.__shot_path+cls.__page_dict["back"], record_pos=(-0.468, -0.248), resolution=(1280, 720)), timeout=cls.__default_timeout) # 返回
         touch(Template(cls.__shot_path+cls.__page_dict["back"], record_pos=(-0.468, -0.248), resolution=(1280, 720))) # 返回
 
     @classmethod
     def cancel(cls):
         """取消"""
+        wait(Template(cls.__shot_path+cls.__page_dict["cancel"], record_pos=(-0.114, 0.216), resolution=(1280, 720), threshold=0.9), timeout=cls.__default_timeout) # 取消
         touch(Template(cls.__shot_path+cls.__page_dict["cancel"], record_pos=(-0.114, 0.216), resolution=(1280, 720), threshold=0.9)) # 取消
 
     @classmethod
     def close(cls):
         """关闭"""
+        wait(Template(cls.__shot_path+cls.__page_dict["close"], threshold=0.9, record_pos=(0.0, 0.218), resolution=(1280, 720)), timeout=cls.__default_timeout) # 关闭
         touch(Template(cls.__shot_path+cls.__page_dict["close"], threshold=0.9, record_pos=(0.0, 0.218), resolution=(1280, 720))) # 关闭
 
     @classmethod
     def white_comfirm(cls):
         """白色确认按钮"""
+        wait(Template(cls.__shot_path+cls.__page_dict["white_comfirm"], record_pos=(-0.001, 0.216), resolution=(1280, 720), threshold=0.9), timeout=cls.__default_timeout) # 白色确认按钮
         touch(Template(cls.__shot_path+cls.__page_dict["white_comfirm"], record_pos=(-0.001, 0.216), resolution=(1280, 720), threshold=0.9)) # 白色确认按钮
 
     @classmethod
     def blue_comfirm(cls):
         """蓝色确认按钮"""
+        wait(Template(cls.__shot_path+cls.__page_dict["blue_comfirm"], record_pos=(0.114, 0.103), resolution=(1280, 720), threshold=0.9), timeout=cls.__default_timeout) # 蓝色确认按钮
         touch(Template(cls.__shot_path+cls.__page_dict["blue_comfirm"], record_pos=(0.114, 0.103), resolution=(1280, 720), threshold=0.9)) # 蓝色确认按钮
 
     @classmethod
     def start_battle(cls):
         """战斗开始"""
+        wait(Template(cls.__shot_path+cls.__page_dict["start_battle"], record_pos=(0.373, 0.191), resolution=(1280, 720)), timeout=cls.__default_timeout)  # 战斗开始
         touch(Template(cls.__shot_path+cls.__page_dict["start_battle"], record_pos=(0.373, 0.191), resolution=(1280, 720)))  # 战斗开始
 
